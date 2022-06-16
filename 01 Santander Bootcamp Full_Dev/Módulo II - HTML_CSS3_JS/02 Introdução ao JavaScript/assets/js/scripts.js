a552974d8692573dd6978071b9a529a28ef240fb
Márcia <marcia.darts@gmail.com>
2022-06-15 21:04:06 -0300

    Introdução ao JS e fork Git Tebs

---
+++ 01 Santander Bootcamp Full_Dev/Módulo II - HTML_CSS3_JS/02 Introdução ao JavaScript/assets/js/scripts.js
@@ -0,0 +1,12 @@
+console.log("hello word!");
+
+
+//funções
+
+function somar (a, b)  {
+    console.log(a + b);
+    return a + b;
+
+}
+
+somar(3,5);
---
+++ 01 Santander Bootcamp Full_Dev/Módulo II - HTML_CSS3_JS/02 Introdução ao JavaScript/teste.js
@@ -0,0 +1,19 @@
+//var y =0; comentário de uma linha
+/*  comentário mais de uma linha   */
+
+var desconto = 0.2;
+const  PRECO =2;
+
+var total = PRECO  - desconto;
+
+//funções
+
+function somar (a, b)  {
+    console.log(a + b);
+    return a + b;
+
+}
+
+somar(3,5);
+
+
