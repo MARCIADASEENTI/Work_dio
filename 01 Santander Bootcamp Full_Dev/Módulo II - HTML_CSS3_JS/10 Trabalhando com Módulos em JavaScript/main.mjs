348bec4fb6e819c028f09a3e776307047b048fdb
Márcia <marcia.darts@gmail.com>
2022-06-27 14:44:19 -0300

    Revisão de Módulos, teste e Certificado

---
+++ 01 Santander Bootcamp Full_Dev/Módulo II - HTML_CSS3_JS/10 Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,7 @@
+import {mostraCidade, mostraIdade,mostraHobby} from "./funcoes.mjs";
+
+
+
+console.log(mostraIdade('Marcia Alves', 50));
+console.log(mostraCidade('Marcia Alves', 'Ibirite'));
+console.log(mostraHobby('Marcia Alves', 'Ouvir Música'));
